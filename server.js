3976aaea79d5e5c13eee2c723606952258380a5b
Andrew <andrewyen64@gmail.com>
2020-07-27 13:51:09 -0700

    Change express.static() to _dirname)

--- server.js
+++ server.js
@@ -13,11 +13,12 @@ var PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Express looks up the files relative to the static directory, so the name of the static directory is not part of the URL.
-app.use(express.static("public"));
+// app.use(express.static('public'));
+app.use(express.static(__dirname));
 
 // ROUTER: Links server.js to the html/api route files
 require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+// require("./routes/htmlRoutes")(app);
 
 
 
