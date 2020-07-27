424ba22b8501438707318299a0ecd392ebe4e8af
Andrew <andrewyen64@gmail.com>
2020-07-27 14:38:00 -0700

    Change route file name

--- server.js
+++ server.js
@@ -13,13 +13,11 @@ var PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Express looks up the files relative to the static directory, so the name of the static directory is not part of the URL.
-// app.use(express.static('public'));
+// app.use(express.static("public"));
 app.use(express.static(__dirname));
 
 // ROUTER: Links server.js to the html/api route files
-require("./routes/apiRoutes")(app);
-// require("./routes/htmlRoutes")(app);
-
+require("./routes/apiAndHTMLRoutes")(app);
 
 
 // LISTENER: Starts the server to begin listening
