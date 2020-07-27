bcdd579b01f17e2a5b484fac5cbb0c88610b370d
Andrew <andrewyen64@gmail.com>
2020-07-27 14:57:39 -0700

    Reorder HTML routes to fix pathing error

--- routes/apiAndHTMLRoutes.js
+++ routes/apiAndHTMLRoutes.js
@@ -4,12 +4,11 @@ var path = require("path");
 
 
 module.exports = function(app) {
-    // GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
+    // Reads the `db.json` file and returns all saved notes as JSON.
     fs.readFile("db/db.json", "utf8", function(error, data) {
         if (error) {
             return console.log(error);
         }
-
         let notesData = JSON.parse(data);
         console.log(notesData);
         
@@ -18,24 +17,22 @@ module.exports = function(app) {
             res.json(notesData);
         });
 
-    // POST `/api/notes` - Should receive a new note to save on the request body, 
-    // add it to the `db.json` file, and then return the new note to the client.
+    // Receives a new note to save on the request body, 
+    // adds it to the 'db.json' file, and then returns the new note to the client.
         app.post("/api/notes", function (req, res) {
             let newNote = req.body;
             notesData.push(newNote);
-            fs.writeFile("db/db.json", JSON.stringify(notesData, "\t"), function(error) {
+            fs.writeFile("db/db.json", JSON.stringify(notesData), function(error) {
                 if (error) {
                     return console.log(error);
                 }
                 return true;
             });
-            // return res.json(notesData)
         });
 
     // DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. 
     // This means you'll need to find a way to give each note a unique `id` when it's saved. 
         app.get("/api/notes/:id", function (req, res) {
-            // display json for the notes array indices of the provided id
             res.json(notesData[req.params.id]);
         });
 
@@ -51,16 +48,15 @@ module.exports = function(app) {
             });
         });
 
-    // HTML routes    
-        // Home page route
-        app.get("*", function (req, res) {
-            res.sendFile(path.join(__dirname, "../public/index.html"));
-        });
+    // HTML routes            
         // Notes page route
         app.get("/notes", function (req, res) {
             res.sendFile(path.join(__dirname, "../public/notes.html"));
         });
-        
+        // Home page route
+        app.get("*", function (req, res) {
+            res.sendFile(path.join(__dirname, "../public/index.html"));
+        });
 
     });
 }
