1021713a61f2806cdf7d1c623a65acc6d1dbf34b
Andrew <andrewyen64@gmail.com>
2020-07-27 16:02:58 -0700

    Remove leftover comments

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
 // Dependencies
 const express = require("express");
-// var path = require("path");
-// const fs = require("fs");
 
 // Sets up the Express App
-var app = express();
+const app = express();
 
 // Sets an initial port
-var PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 // Sets up the Express app to Handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Express looks up the files relative to the static directory, so the name of the static directory is not part of the URL.
-// app.use(express.static("public"));
+// Express looks up the files relative to the static directory, so that css/js files will run properly.
 app.use(express.static(__dirname));
 
 // ROUTER: Links server.js to the html/api route files
