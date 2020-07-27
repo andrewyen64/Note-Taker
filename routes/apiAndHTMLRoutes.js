d8fb7078c63e202731417608656cd857663e50b8
Andrew <andrewyen64@gmail.com>
2020-07-27 16:30:50 -0700

    Add console.logs to guide progress and wrote descriptive comments

--- routes/apiAndHTMLRoutes.js
+++ routes/apiAndHTMLRoutes.js
@@ -4,21 +4,21 @@ var path = require("path");
 
 
 module.exports = function(app) {
-    // Reads the `db.json` file and returns all saved notes as JSON.
+    // Reads the 'db.json' file and returns all saved notes as JSON.
     fs.readFile("db/db.json", "utf8", function(error, data) {
         if (error) {
             return console.log(error);
         }
         let notesData = JSON.parse(data);
-        console.log(notesData);
+        console.log("Current List of All Notes: ", notesData);
         
     // Api routes
         app.get("/api/notes", function (req, res) {
             res.json(notesData);
         });
 
-    // Receives a new note to save on the request body, 
-    // adds it to the 'db.json' file, and then returns the new note to the client.
+        // Receives new note to save on the request body, 
+        // adds it to the 'db.json' file, and then returns the new note to the client.
         app.post("/api/notes", function (req, res) {
             let newNote = req.body;
             notesData.push(newNote);
@@ -28,24 +28,34 @@ module.exports = function(app) {
                 }
                 return true;
             });
+            console.log("============================================================");
+            console.log("Posted newNote to api/notes: ", newNote)
+            console.log("============================================================");
+            console.log("Updated List of All Notes: ", 
+            notesData);
         });
 
-    // DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. 
-    // This means you'll need to find a way to give each note a unique `id` when it's saved. 
+        // Receives a query parameter containing the id of a note to delete. 
+        // This means you'll need to find a way to give each note a unique `id` when it's saved. 
         app.get("/api/notes/:id", function (req, res) {
             res.json(notesData[req.params.id]);
         });
 
-    // In order to delete a note, you'll need to read all notes from the `db.json` file, 
-    // remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+        // Finds note with specific id and removes that note from the notesData array,
+        // then rewrites the notes to the 'db.json' file.
         app.delete("/api/notes/:id", function (req, res) {
             notesData.splice(req.params.id, 1);
-            fs.writeFile("db/db.json", JSON.stringify(notesData, "\t"), function(error) {
+            fs.writeFile("db/db.json", JSON.stringify(notesData), function(error) {
                 if (error) {
                     return console.log(error);
                 }
                 return true;
             });
+            console.log("============================================================");
+            console.log("Deleted selected note from api/notes")
+            console.log("============================================================");
+            console.log("Updated List of All Notes: ", 
+            notesData);
         });
 
     // HTML routes            
